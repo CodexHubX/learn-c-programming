a4451b7b6694eded50b273ee4a0c0a2fabdc8a71
CodexHubX <111242276+CodexHubX@users.noreply.github.com>
2026-05-09 20:57:52 +0700

    Add hello world and fizzbuzz

---
+++ week01/hello_world/Project1/main.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS // ปิด warning ของ Microsoft ที่บอกว่าฟังก์ชันบางตัว (เช่น scanf, fopen, strcpy) "ไม่ปลอดภัย"
+#define MAX_PLAYERS 100 // ประกาศคงที่ คล้ายๆ cost
+
+#include <stdio.h>
+
+int main(void) {
+	printf("Hellow World\n");
+	printf("I will become a kernel hacker.\n");
+	printf("Player : %d\n", MAX_PLAYERS);
+	return 0;
+}
